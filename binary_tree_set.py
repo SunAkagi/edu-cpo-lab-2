1f68b4f8130c697d2df383172968957776c5127b
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-09 07:10:44 +0800

    Update binary_tree_set.py

--- binary_tree_set.py
+++ binary_tree_set.py
@@ -54,8 +54,8 @@ class BinaryTreeSet(Generic[KT, VT]):
             yield from self.right
 
 
-def empty() -> BinaryTreeSet[KT, VT]:
-    return BinaryTreeSet(KT(), VT())
+def empty() -> Optional[BinaryTreeSet[KT, VT]]:
+    return None
 
 
 def cons(
