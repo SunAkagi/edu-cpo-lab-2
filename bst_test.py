0f45c6ed8feb902293500f6071a8e1aa0ac18110
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-09 18:59:17 +0800

    Update bst_test.py

--- bst_test.py
+++ bst_test.py
@@ -3,7 +3,6 @@ import itertools
 from binary_tree_set import concat, cons, from_list, \
     intersection, length, member, remove, to_list, empty, map_set, \
     filter_set, reduce_set
-from typing import Tuple
 from hypothesis import given, strategies as st
 
 
