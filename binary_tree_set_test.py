c4287a328efd87f58bac3c14b18d985325643937
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-15 19:34:36 +0800

    Update binary_tree_set_test.py

--- binary_tree_set_test.py
+++ binary_tree_set_test.py
@@ -2,7 +2,7 @@ import itertools
 
 from binary_tree_set import concat, cons, from_list, \
     intersection, length, member, remove, to_list, empty, map_set, \
-    filter_set, reduce_set, tree_equal_structure
+    filter_set, reduce_set, tree_equal_structure, BinaryTreeSet
 from hypothesis import given, strategies as st
 from typing import List, Tuple
 
