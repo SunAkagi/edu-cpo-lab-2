97e2283b948da7e5916a0b963237342c3fef3f04
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-15 18:54:25 +0800

    Update binary_tree_set.py

--- binary_tree_set.py
+++ binary_tree_set.py
@@ -90,7 +90,7 @@ def cons(
 
 def from_list(pairs: List[Tuple[KT, VT]]) -> BinaryTreeSet[KT, VT]:
     tree: BinaryTreeSet[KT, VT] = empty()
-    for k, v in sorted(pairs):
+    for k, v in pairs:
         tree = cons((k, v), tree)
     return tree
 
@@ -139,7 +139,7 @@ def concat(
     a: BinaryTreeSet[KT, VT],
     b: BinaryTreeSet[KT, VT]
 ) -> BinaryTreeSet[KT, VT]:
-    all_pairs = to_list(a) + to_list(b)
+    all_pairs = sorted(to_list(a) + to_list(b))
     return from_list(all_pairs)
 
 
