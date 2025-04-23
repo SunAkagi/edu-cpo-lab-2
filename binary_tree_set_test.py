3e21b426f8b011ecfd6d6f20c1172e4d76d16d83
YANGLiang234 <103995785+YANGLiang234@users.noreply.github.com>
2025-04-24 04:06:50 +0800

    Update binary_tree_set_test.py

--- binary_tree_set_test.py
+++ binary_tree_set_test.py
@@ -1,8 +1,9 @@
 import itertools
 
-from binary_tree_set import concat, cons, from_list, \
+from binary_tree_set BinaryTreeSet, import concat, cons, from_list, \
     intersection, length, member, remove, to_list, empty, map_set, \
     filter_set, reduce_set
+from typing import Optional
 from hypothesis import given, strategies as st
 
 
